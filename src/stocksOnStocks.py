80fd232aaa025ccdcf4d1498e9c3f484cbe6ed1f
ryoheinamiki515 <ryoheinamiki515@gmail.com>
2019-03-02 17:02:52 -0600

    with modules

---
+++ src/stocksOnStocks.py
@@ -0,0 +1,11 @@
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+
+symbols = ['NKE', 'AAPL', 'NFLX', 'MSFT', ]
+
+new_df = pd.DataFrame(columns=symbols)
+for stock in symbols:
+    ts = TimeSeries(key='KU7YQYX4LFOFNQTU', output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')
+    new_df[stock] = data['4. close']
+print(new_df)
