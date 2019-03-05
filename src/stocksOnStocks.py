1a5719378b03c6ac4070467bd6c39093690c73a6
ryoheinamiki515 <ryoheinamiki515@gmail.com>
2019-03-04 22:41:58 -0600

    email and EMA (email doesn't work yet)

--- src/stocksOnStocks.py
+++ src/stocksOnStocks.py
@@ -9,3 +9,7 @@ for stock in symbols:
     ts = TimeSeries(key='KU7YQYX4LFOFNQTU', output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')
     new_df[stock] = data['4. close']
+
+print(new_df)
+
+
