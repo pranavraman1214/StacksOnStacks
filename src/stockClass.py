d8c4299344dd8bdc6fca3769bf99ed2104d9d2d9
pranavraman <pvraman2@illinois.edu>
2019-03-05 14:18:14 -0600

    Inserted comments and added more stocks

--- src/stockClass.py
+++ src/stockClass.py
@@ -1,66 +1,71 @@
 from alpha_vantage.techindicators import TechIndicators
 import smtplib
 class indicateStock:
-    def __init__(self, symbol):
+    def __init__(self, symbol, increasing):
         self.symbol = symbol
-
-    def getEMA50(self):
+        self.increasing = increasing
+    def getEMA12(self):
         whatever = None
         while whatever is None:
             try:
                 ti = TechIndicators(key='KU7YQYX4LFOFNQTU', output_format='pandas')
-                data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='50', series_type='close')
+                data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='12', series_type='close')
                 whatever = data
-                ema50 = float(data.iloc[data.shape[0] - 1])
                 return float(data.iloc[data.shape[0] - 1])
             except KeyError:
                 pass
-
-    def getEMA200(self):
+    def getEMA26(self):
         whatever = None
         while whatever is None:
             try:
                 ti = TechIndicators(key='KU7YQYX4LFOFNQTU', output_format='pandas')
-                data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='200', series_type='close')
+                data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='26', series_type='close')
                 whatever = data
                 return float(data.iloc[data.shape[0] - 1])
             except KeyError:
                 pass
-
     def compareEMA50and200(self):
-        increasing = False
-        ema50 = self.getEMA50()
-        ema200 = self.getEMA200()
+        ema12 = self.getEMA12()
+        ema26 = self.getEMA26()
         print self.symbol
-        print ema200
-        print ema50
-        if ema200 > ema50:
-            increasing = True
-        if ema50 < ema200:
-            increasing = False
-        if ema200 == ema50:
-            if increasing == True:
+        print ema26
+        print ema12
+        if ema26 > ema12:
+            self.increasing = True
+            print self.increasing
+        if ema26 < ema12:
+            self.increasing = False
+            print self.increasing
+        if self.increasing == True:
+            s = smtplib.SMTP("smtp.gmail.com", 587)
+            s.starttls()
+            s.ehlo
+            s.login("pranavryoheistocksonstocks@gmail.com", "rnamiki2pvraman2")
+            s.sendmail("pranavryoheistocksonstocks@gmail.com", "pranavryoheistocksonstocks@gmail.com", "Buy the stock " + self.symbol)
+            s.quit()
+        if ema12 == ema26:
+            if self.increasing == True:
                 s = smtplib.SMTP("smtp.gmail.com", 587)
                 s.starttls()
                 s.ehlo
                 s.login("pranavryoheistocksonstocks@gmail.com", "rnamiki2pvraman2")
-                s.sendmail("pranavryoheistocksonstocks@gmail.com", "pvraman2@illinois.edu", "Buy the stock" + self.symbol)
+                s.sendmail("pranavryoheistocksonstocks@gmail.com", "pranavryoheistocksonstocks@gmail.com","Buy the stock " + self.symbol)
                 s.quit()
             else:
                 s = smtplib.SMTP("smtp.gmail.com", 587)
                 s.starttls()
                 s.ehlo
                 s.login("pranavryoheistocksonstocks@gmail.com", "rnamiki2pvraman2")
-                s.sendmail("pranavryoheistocksonstocks@gmail.com", "pvraman2@illinois.edu", "Sell the stock" + self.symbol)
+                s.sendmail("pranavryoheistocksonstocks@gmail.com", "pranavryoheistocksonstocks@gmail.com","Sell the stock" + self.symbol)
                 s.quit()
 
 # Sets up the stocks. Basically we could place all the stocks from robin hood in this array
-symbols = ['NKE', 'AAPL', 'NFLX', 'MSFT', ]
+allsymbols = ['NKE', 'AAPL', 'NFLX', 'MSFT', 'TRXC', 'SNAP', 'ACB']
 stockObject = []
 stock = 0
 # Cycles thru and makes a Stock object of each symbol
-for x in range(len(symbols)):
-    stockObject.append(indicateStock(symbols[x]))
+for x in range(len(allsymbols)):
+    stockObject.append(indicateStock(allsymbols[x], False))
 # Infinite loop of comparisons
 i = 0
 while i is 0:
