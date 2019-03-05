67ee6adb30f7df0b49074faf5ed3a755fed022b7
ryoheinamiki515 <ryoheinamiki515@gmail.com>
2019-03-05 16:35:08 -0600

    list of stock symbols

--- src/stockClass.py
+++ src/stockClass.py
@@ -1,29 +1,35 @@
 from alpha_vantage.techindicators import TechIndicators
 import smtplib
+import pandas as pd
+
+
 class indicateStock:
     def __init__(self, symbol, increasing):
         self.symbol = symbol
         self.increasing = increasing
+
     def getEMA12(self):
         whatever = None
         while whatever is None:
             try:
-                ti = TechIndicators(key='KU7YQYX4LFOFNQTU', output_format='pandas')
+                ti = TechIndicators(key='JMCHDT9XJ90DOIQL', output_format='pandas')
                 data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='12', series_type='close')
                 whatever = data
                 return float(data.iloc[data.shape[0] - 1])
             except KeyError:
                 pass
+
     def getEMA26(self):
         whatever = None
         while whatever is None:
             try:
-                ti = TechIndicators(key='KU7YQYX4LFOFNQTU', output_format='pandas')
+                ti = TechIndicators(key='JMCHDT9XJ90DOIQL', output_format='pandas')
                 data, metadata = ti.get_ema(symbol=self.symbol, interval='1min', time_period='26', series_type='close')
                 whatever = data
                 return float(data.iloc[data.shape[0] - 1])
             except KeyError:
                 pass
+
     def compareEMA50and200(self):
         ema12 = self.getEMA12()
         ema26 = self.getEMA26()
@@ -36,7 +42,7 @@ class indicateStock:
         if ema26 < ema12:
             self.increasing = False
             print(self.increasing)
-        if self.increasing == True:
+        if self.increasing:
             s = smtplib.SMTP("smtp.gmail.com", 587)
             s.starttls()
             s.ehlo
@@ -44,7 +50,7 @@ class indicateStock:
             s.sendmail("pranavryoheistocksonstocks@gmail.com", "pranavryoheistocksonstocks@gmail.com", "Buy the stock " + self.symbol)
             s.quit()
         if ema12 == ema26:
-            if self.increasing == True:
+            if self.increasing:
                 s = smtplib.SMTP("smtp.gmail.com", 587)
                 s.starttls()
                 s.ehlo
@@ -59,8 +65,10 @@ class indicateStock:
                 s.sendmail("pranavryoheistocksonstocks@gmail.com", "pranavryoheistocksonstocks@gmail.com","Sell the stock" + self.symbol)
                 s.quit()
 
+
 # Sets up the stocks. Basically we could place all the stocks from robin hood in this array
-allsymbols = ['NKE', 'AAPL', 'NFLX', 'MSFT', 'TRXC', 'SNAP', 'ACB']
+df = pd.read_csv("C:/Users/ryohe/PycharmProjects/StacksOnStacks/stockSymbols.csv")
+allsymbols = list(df["Symbols"])
 stockObject = []
 stock = 0
 # Cycles thru and makes a Stock object of each symbol
