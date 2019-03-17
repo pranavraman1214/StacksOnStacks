43093d184ed0a6a80509ed6b54e2c13ebf3bd746
pranavraman <pvraman2@illinois.edu>
2019-03-17 02:27:52 -0500

    Correction. Now I got it

--- src/webScrappingStocks.py
+++ src/webScrappingStocks.py
@@ -6,16 +6,18 @@ key_stats_on_main =['Market Cap', 'PE Ratio (TTM)', 'EPS (TTM)']
 key_stats_on_stat =['Enterprise Value', 'Trailing P/E', 'Forward P/E',
                      'PEG Ratio (5 yr expected)', 'Return on Assets', 'Quarterly Revenue Growth',
                      'EBITDA', 'Diluted EPS', 'Total Debt/Equity', 'Current Ratio']
+driver = webdriver.Chrome
 
 page_link = "https://finance.yahoo.com/quote/SIRI/key-statistics?p=SIRI"
-page_response = requests.get(page_link, timeout=5)
+page_response = requests.get(page_link, driver, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
 textContent = []
 data = []
-searchString = "tr"
+searchString = "td"
 for i in range(len(page_content.find_all(searchString))):
     paragraphs = page_content.find_all(searchString)[i].text
     textContent.append(paragraphs)
+
 for name in textContent:
     print (name)
 
